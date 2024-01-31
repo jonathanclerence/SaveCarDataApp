159759170ace30bf6972de115d73fc89b140cd23
Jonathan Clerence <johanclerence@gmail.com>
2024-01-31 20:40:13 +0530

    Add project files.

---
+++ SaveCarDataApp/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaveCarDataApp.Models;
+
+namespace SaveCarDataApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected IConfiguration Configuration;
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnectionString"));
+        }
+
+        public DbSet<Car> Cars { get; set; }    
+    }
+}
---
+++ SaveCarDataApp/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaveCarDataApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public bool SpareWheel { get; set; }
+
+    }
+}
